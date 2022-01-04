fac4b784d229eade854fa3c6d8f16f9cfc6a7a9c
jvj13122010 <80154440+jvj13122010@users.noreply.github.com>
2022-01-04 17:52:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup() {
+    console.log("Setup Complete!");
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("gainsboro");
+}
+
+function clearCanvas() {
+    background("gainsboro");
+}
